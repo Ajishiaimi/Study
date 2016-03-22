650189e241984473d4a2581a56f24d0169a707da
Ajishiaimi <ajishiaimi@gmail.com>
2016-03-23 04:56:38 +0900

    3/23 #10 対応
    #10

--- App_Code/FB_Const.vb
+++ App_Code/FB_Const.vb
@@ -1,8 +1,11 @@
-﻿Namespace FizzBuzz_Const
+﻿Namespace FizzBuzzConst
 
-    Public Class FB_Const
+    Public MustInherit Class Values
         Public Const 初期値 As Integer = 1
         Public Const 最大値 As Integer = 30
+    End Class
+
+    Public MustInherit Class Text
         Public Const 出力結果_Fizz As String = "Fizz"
         Public Const 出力結果_Buzz As String = "Buzz"
         Public Const 出力結果_FizzBuzz As String = "FizzBuzz"
