6eaa7f8a8c6d2cdb93c37f668e0464b5d5c63a4c
Ajishiaimi <ajishiaimi@gmail.com>
2016-03-28 03:54:24 +0900

    3/28 #16 対応
    で、できた・・・！
    リピーターに対応したaspx/vb追加

---
+++ App_Code/Holder.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Holder
+
+    Private myNumberOutput As String
+    Private myResultOutput As String
+
+    Public Sub New(newNumberOutput As String, newResultOutput As String)
+        Me.myNumberOutput = newNumberOutput
+        Me.myResultOutput = newResultOutput
+    End Sub
+
+    Public ReadOnly Property NumberOutput() As String
+        Get
+            Return myNumberOutput
+        End Get
+    End Property
+
+    Public ReadOnly Property ResultOutput() As String
+        Get
+            Return myResultOutput
+        End Get
+    End Property
+
+End Class
---
+++ FizzBuzz_Repeter有.aspx.vb
@@ -0,0 +1,54 @@
+﻿Option Explicit On
+
+Imports FizzBuzzConst
+
+Partial Class FizzBuzz
+    Inherits Page
+
+    ''' <summary>
+    ''' ポチっとなボタン押下時
+    ''' </summary>
+    Protected Sub btn1_Click(sender As Object, e As EventArgs) Handles btnFizzBuzz.Click
+        Dim ResultOutput As String = ""
+        Dim NumberOutput As String = ""
+        Dim MinNumber As Integer = FizzBuzzConst.Values.初期値
+        Dim MaxNumber As Integer = FizzBuzzConst.Values.最大値
+
+        ' TODO:テキストボックスの入力チェックを実装する
+        If Not String.IsNullOrEmpty(Me.txtNumber.Text) Then
+            MaxNumber = Integer.Parse(Me.txtNumber.Text)
+        End If
+
+        Dim OutoutValues As New ArrayList()
+        For Number As Integer = MinNumber To MaxNumber
+            Dim Bit As Integer = 0
+            If Number Mod 3 = 0 Then
+                Bit += 2 ^ 1
+            End If
+            If Number Mod 5 = 0 Then
+                Bit += 2 ^ 2
+            End If
+            Select Case Bit
+                Case 2
+                    OutoutValues.Add(New Holder(Number, FizzBuzzConst.Text.出力結果_Fizz))
+                Case 4
+                    OutoutValues.Add(New Holder(Number, FizzBuzzConst.Text.出力結果_Buzz))
+                Case 6
+                    OutoutValues.Add(New Holder(Number, FizzBuzzConst.Text.出力結果_FizzBuzz))
+                Case Else
+                    OutoutValues.Add(New Holder(Number, Number))
+            End Select
+        Next
+        Me.Repeter1.DataSource = OutoutValues
+        Me.Repeter1.DataBind()
+    End Sub
+
+    ''' <summary>
+    ''' クリアボタン押下
+    ''' </summary>
+    Protected Sub btn_c_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Repeter1.DataSource = Nothing
+        Repeter1.DataBind()
+    End Sub
+
+End Class
